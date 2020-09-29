fe8a393da088b3fc06d199462c003c44c78d53eb
RahulChowdhury20 <59206706+RahulChowdhury20@users.noreply.github.com>
2020-09-29 10:20:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+//document.querySelector(".w").addEventListener("click",handleclick);//no parenthesis in function
+//document.querySelector(".a").addEventListener("click",handleclick);
+//document.querySelector(".s").addEventListener("click",handleclick);
+//document.querySelector(".d").addEventListener("click",handleclick);
+//document.querySelector(".j").addEventListener("click",handleclick);
+//document.querySelector(".k").addEventListener("click",handleclick);
+//document.querySelector(".l").addEventListener("click",handleclick);
+
+//or
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+
+var buttonInnerHTML = this.innerHTML;
+makesound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+
+}); 
+}
+
+document.addEventListener("keypress",function(event){
+	makesound(event.key);
+	buttonAnimation(event.key);
+});
+
+function makesound(key)
+{
+  switch(key)
+{
+	case "w":
+     var tom1 = new Audio('sounds/tom-1.mp3');
+	 tom1.play();
+	break;
+	case "a":
+     var tom2 = new Audio('sounds/tom-2.mp3');
+	 tom2.play();
+	break;
+    case "s":
+     var tom3 = new Audio('sounds/tom-3.mp3');
+	 tom3.play();
+	break;
+    case "d":
+     var tom4 = new Audio('sounds/tom-4.mp3');
+	 tom4.play();
+	break;
+    case "j":
+     var snare = new Audio('sounds/snare.mp3');
+	 snare.play();
+	break;
+	case "k":
+     var crash = new Audio('sounds/crash.mp3');
+	 crash.play();
+	break;
+	case "l":
+     var kick = new Audio('sounds/kick-bass.mp3');
+	 kick.play();
+	break;
+
+    default: console.log(buttonInnerHTML);
+
+
+
+
+	 
+
+}
+
+
+
+}
+
+function buttonAnimation(currentkey)
+{
+   var activeButton = document.querySelector("." + currentkey);
+   activeButton.classList.add("pressed");
+
+setTimeout(function(){
+	activeButton.classList.remove("pressed");
+}, 100);
+}
+//setTimeout(function,milliseconds,parametre1,parametre2,..)
+//var audio = new Audio('sounds/tom-1.mp3');
+//	audio.play();
